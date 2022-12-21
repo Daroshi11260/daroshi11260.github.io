a89f684a8be44c90826fe4b583fb8b7aaec51168
Daroshi11260 <37487623+Daroshi11260@users.noreply.github.com>
2022-12-21 05:39:36 +0000

    Deploying to gh-pages from @ Daroshi11260/daroshi11260.github.io@aaccc5b693e69c1e83bc657e69ff6facdac53c8d 🚀

--- _next/static/sPBnobwb1HRFQW-lkYOym/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ae0e3e027412e072.css","static/chunks/pages/index-bdb70f1696b1874a.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
